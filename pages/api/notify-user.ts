4991de6f7c99a736f924e0353d6ac8a37b9ddac7
justindi15 <justindi15@yahoo.com>
2021-08-21 18:19:38 -0700

    add fetch-subscriptions and notify-user endpoints

---
+++ pages/api/notify-user.ts
@@ -0,0 +1,26 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { sendHasRestockedEmail } from '../../server/email'
+import { fetchSubscription } from '../../server/firestore'
+import { sendHasRestockedText } from '../../server/text'
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+    switch (req.method) {
+        case 'POST':
+            return notifyUserAndDeleteSubscription(req, res)
+        default:
+            return res.status(404).send('Request handler not found')
+    }
+}
+
+const notifyUserAndDeleteSubscription = async (req: NextApiRequest, res: NextApiResponse) => {
+    const { subscription } = req.body
+    try {
+        await sendHasRestockedEmail(subscription)
+        await sendHasRestockedText(subscription)
+        const sub = await fetchSubscription(subscription.id)
+        await sub.ref.delete()
+        res.json(`Sent notification for ${subscription}`)
+    } catch (error) {
+        res.json({error: error.message})
+    }
+}
